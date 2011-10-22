03e0554d27544214c0b206067e484d53e016d0e5
Michael Single <silent.simplay@gmail.com>
2011-10-22 20:30:18 +0200

    refactoring getNextRepetitionEvent() for monthly events

--- calendar/app/models/Event.java
+++ calendar/app/models/Event.java
@@ -90,30 +90,79 @@ public class Event implements Comparable<Event> {
 		return this.intervall;
 	}
 
-	//TODO: fix ugly date instantiation
+	//TODO: fix ugly date instantiation and fix correct calculation for monthly repeating events
 	public Event getNextRepetitionEvent() {
 		Date nextRepStartDate = new Date(start.getYear(), start.getMonth(), start.getDate() + intervall, start.getHours(), start.getMinutes());
 		Date nextRepEndDate = new Date(end.getYear(), end.getMonth(), end.getDate() + intervall, start.getHours(), start.getMinutes());
+		
 		if (intervall == 30) {
 			
 			// get month of start to be corrected: add a extra variable for end.getMonth()
 			int k = start.getMonth()+1; 
 			int delta = 0;
+			int yearType = (start.getYear() +1900)%4;
+			// leap year: yearType = 0;
+			// normal year: yearType = 1|2|3;
+			int month = start.getMonth()+1;
 			
+			
+			// if it is jan, mar, mai, jun, jul, okt or dez ==> 31er months
+			if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+				
+				// if we have a event on a 31th of month, then feb, apr, aug, sep, nov wont have an event, 
+				// since they have no 31th. Therefore +2
+				if(start.getDate()==31){
+					
+					// if we have december or june
+					if(k==7 || k==12) delta = -1; 
+					
+					nextRepStartDate = new Date(start.getYear(), start.getMonth()+2+delta, start.getDate(), start.getHours(), start.getMinutes());
+					nextRepEndDate = new Date(end.getYear(), end.getMonth()+2+delta, end.getDate(), start.getHours(), start.getMinutes());
+				}else{
+					
+				}
+				
+			// if it is feb ==> 28er or 29er month
+			}else if(month == 2){
+				// if we have a leap year
+				if(yearType == 0){
+					
+				}else{
+					
+				}
+				
+				
+			// if it is apr, aug, sep or nov => 30er months
+			}else{
+				
+			}
+			
+			
+			/*
 			// if we have a month with 31 days as rep. event marked
 			if(start.getDate()==31){ 
 				
-				// if we have december or june
-				if(k==7 || k==12)delta = -1; 
-				nextRepStartDate = new Date(start.getYear(), start.getMonth()+2+delta, start.getDate(), start.getHours(), start.getMinutes());
-				nextRepEndDate = new Date(end.getYear(), end.getMonth()+2+delta, end.getDate(), start.getHours(), start.getMinutes());
+				// if we have december or july
+				if(k==7 || k==12)delta = 1; 
+				
+				nextRepStartDate = new Date(start.getYear(), start.getMonth()+1, 30, start.getHours(), start.getMinutes());
+				//System.out.println(nextRepStartDate);
+				nextRepEndDate = new Date(end.getYear(), end.getMonth()+1, 30, end.getHours(), end.getMinutes());
 			
 			// if we have a leap year
 			}else if(start.getDate()==29 && start.getMonth() == 1){
 				nextRepStartDate = new Date(start.getYear()+4, start.getMonth(), start.getDate(), start.getHours(), start.getMinutes());
 				nextRepEndDate = new Date(end.getYear()+4, end.getMonth(), end.getDate(), start.getHours(), start.getMinutes());
+			
+			}else if(start.getDate()==30){
+				
 			}
 			
+			
+			
+			//nextRepStartDate = new Date(start.getYear(), start.getMonth()+1, start.getDate(), start.getHours(), start.getMinutes());
+			//nextRepEndDate = new Date(end.getYear(), end.getMonth()+1, end.getDate(), start.getHours(), start.getMinutes());
+			*/
 		}
 		if (intervall == 365) {	
 			// if we have a leap year, remember february is equals 1
