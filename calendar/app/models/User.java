a2cfb8563acb5c3608559126b6f544c35ed33638
Francis Bruehlmann <francis.brue@me.com>
2011-11-08 22:43:40 +0100

    Test coverage of 73% for the user and 59.4% for the Event. I also tested the get and set methods... I know, I should not have done this but it was funny ;-)

--- calendar/app/models/User.java
+++ calendar/app/models/User.java
@@ -2,29 +2,29 @@ package models;
 
 import java.util.LinkedList;
 
-import org.joda.time.DateTime;
-
 import models.Event.Visibility;
 
+import org.joda.time.DateTime;
 
 /**
- * The User class represents a User of this Calendar application.
- * Users may have multiple Calendars, all of which can contain multiple Events.
- * Users are responsible for maintaining the calendar.
+ * The User class represents a User of this Calendar application. Users may have
+ * multiple Calendars, all of which can contain multiple Events. Users are
+ * responsible for maintaining the calendar.
  * 
  * @see {@link Calendar}
- *
+ * 
  */
 public class User {
 	public String name;
 	public LinkedList<Calendar> calendar;
-	
-	// in this list we store all calendars of other user which we want to display in our calendar
+
+	// in this list we store all calendars of other user which we want to
+	// display in our calendar
 	public LinkedList<Calendar> observedCalendars;
 	public LinkedList<Long> shownObservedCalendars;
 	public String password;
 	public Event birthday;
-//	public boolean isPublicBirthday;
+	// public boolean isPublicBirthday;
 	public long id;
 	private String nickname;
 	private Calendar birthdayCalendar;
@@ -43,10 +43,14 @@ public class User {
 	/**
 	 * Create a new User for this calendar application.
 	 * 
-	 * @param name The Users name.
-	 * @param password The Users password.
-	 * @param birthday The Users birthday.
-	 * @param nickname The Users nickname.
+	 * @param name
+	 *            The Users name.
+	 * @param password
+	 *            The Users password.
+	 * @param birthday
+	 *            The Users birthday.
+	 * @param nickname
+	 *            The Users nickname.
 	 * @see {@link User}
 	 */
 	public User(String name, String password, DateTime birthday, String nickname) {
@@ -63,12 +67,14 @@ public class User {
 		this.name = name;
 		this.nickname = nickname;
 		this.password = password;
-		this.birthday = new Event(this, birthday, birthday, name + "'s birthday", Visibility.PRIVATE, true, 365, this.birthdayCalendar.getId(), false);
+		this.birthday = new Event(this, birthday, birthday, name
+				+ "'s birthday", Visibility.PRIVATE, true, 365,
+				this.birthdayCalendar.getId(), false);
 		counter++;
 		this.id = counter;
 
 		birthdayCalendar.addEvent(this.birthday);
-		
+
 		// each user x has a default a calender called: x's first calendar
 		calendar.add(new Calendar(name + "'s first calendar", this));
 
@@ -76,9 +82,10 @@ public class User {
 		assert this.name.equals(name);
 		assert calendar != null;
 	}
-	
+
 	/**
 	 * Get this Users password.
+	 * 
 	 * @return The <code>password</code> of this User.
 	 */
 	public String getPassword() {
@@ -87,7 +94,9 @@ public class User {
 
 	/**
 	 * Set this Users password
-	 * @param password The password to be set.
+	 * 
+	 * @param password
+	 *            The password to be set.
 	 */
 	public void setPassword(String password) {
 		this.password = password;
@@ -95,22 +104,26 @@ public class User {
 
 	/**
 	 * Get this Users name.
+	 * 
 	 * @return The <code>name</code> of this User.
 	 */
 	public String getName() {
 		return this.name;
 	}
-	
+
 	/**
 	 * Set this Users name.
-	 * @param name The name to be set.
+	 * 
+	 * @param name
+	 *            The name to be set.
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	/**
 	 * Get this Users birthday.
+	 * 
 	 * @return The <code>birthday</code> of this User.
 	 */
 	public Event getBirthday() {
@@ -119,51 +132,63 @@ public class User {
 
 	/**
 	 * Set this Users birthday date.
-	 * @param birthdayDate The new date of the Users <code>birthday</code>.
+	 * 
+	 * @param birthdayDate
+	 *            The new date of the Users <code>birthday</code>.
 	 */
 	public void setBirthdayDate(DateTime birthdayDate) {
-		birthday.edit(birthdayDate, birthdayDate, birthday.name, birthday.visibility, true, 365);
+		birthday.edit(birthdayDate, birthdayDate, birthday.name,
+				birthday.visibility, true, 365);
 	}
-	
+
 	/**
 	 * Returns the visibility status of the Users birthday.
-	 * @return <code>true</code> if this Users birthday is either Public or Busy.
-	 * <code>false</code> otherwise.
+	 * 
+	 * @return <code>true</code> if this Users birthday is either Public or
+	 *         Busy. <code>false</code> otherwise.
 	 * @see {@link Visibility}
 	 */
 	public boolean isBirthdayPublic() {
 		return birthday.isVisible();
 	}
-	
+
 	/**
 	 * Set this Users birthday visible or hidden for other Users.
-	 * @param is_visible If true, the birthday will be set to visible. If false, the birthday will be set to private.
+	 * 
+	 * @param is_visible
+	 *            If true, the birthday will be set to visible. If false, the
+	 *            birthday will be set to private.
 	 */
 	public void setBirthdayPublic(boolean is_visible) {
-		Visibility visibility = is_visible ? Visibility.PUBLIC : Visibility.PRIVATE;
-		birthday.edit(birthday.start, birthday.end, birthday.name, visibility, true, 365);
+		Visibility visibility = is_visible ? Visibility.PUBLIC
+				: Visibility.PRIVATE;
+		birthday.edit(birthday.start, birthday.end, birthday.name, visibility,
+				true, 365);
 		System.out.println("changed birthday to : " + visibility);
 	}
-	
+
 	/**
 	 * Get this Users nickname.
+	 * 
 	 * @return The <code>nickname</code> of this User.
 	 */
 	public String getNickname() {
 		return this.nickname;
 	}
-	
+
 	/**
 	 * Set this Users <code>nickname</code>.
-	 * @param nickname The nickname to be set.
+	 * 
+	 * @param nickname
+	 *            The nickname to be set.
 	 */
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
 
-
 	/**
 	 * Return all calendars of a user
+	 * 
 	 * @return A list of all Calendars of this User.
 	 * @see {@link Calendar}
 	 */
@@ -173,6 +198,7 @@ public class User {
 
 	/**
 	 * Get this Users default calendar.
+	 * 
 	 * @return The first Calendar of this User.
 	 */
 	public Calendar getdefaultCalendar() {
@@ -182,10 +208,14 @@ public class User {
 	/**
 	 * Get a Calendar from this Users list of Calendars based on its Id.
 	 * 
-	 * Iterate over all Calendars in <code>calendars</code>. If a Calendar has the same id as the argument, it will be returned.
-	 * @param calId The Id to compare this Users Calendars with.
-	 * @return <code>null</code> if no Calendar in <code>calendars</code> has the same id as the argument.
-	 * A Calendar with the same id as the argument otherwise.
+	 * Iterate over all Calendars in <code>calendars</code>. If a Calendar has
+	 * the same id as the argument, it will be returned.
+	 * 
+	 * @param calId
+	 *            The Id to compare this Users Calendars with.
+	 * @return <code>null</code> if no Calendar in <code>calendars</code> has
+	 *         the same id as the argument. A Calendar with the same id as the
+	 *         argument otherwise.
 	 */
 	public Calendar getCalendarById(long calId) {
 		Calendar result = null;
@@ -195,51 +225,65 @@ public class User {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Add a new Calendar to this Users list of Calendars.
-	 * @param cal The Calendar to be added to <code>calendars</code> .
+	 * 
+	 * @param cal
+	 *            The Calendar to be added to <code>calendars</code> .
 	 */
 	public void addCalendar(Calendar cal) {
 		calendar.add(cal);
 	}
-	
+
 	/**
-	 * Add a new Calendar of another user to this Users observed calendar list. 
-	 * @param cal The Calendar to be added to <code>observedCalendars</code>.
+	 * Add a new Calendar of another user to this Users observed calendar list.
+	 * 
+	 * @param cal
+	 *            The Calendar to be added to <code>observedCalendars</code>.
 	 */
-	public void addObservedCalendar(Calendar cal){
+	public void addObservedCalendar(Calendar cal) {
 		observedCalendars.add(cal);
 	}
+
 	/**
-	 * Remove a Calendar of another user from this Users observed Calendar list. 
-	 * @param cal The Calendar to be removed to <code>observedCalendars</code>.
+	 * Remove a Calendar of another user from this Users observed Calendar list.
+	 * 
+	 * @param cal
+	 *            The Calendar to be removed to <code>observedCalendars</code>.
 	 */
-	public void removeObservedCalendar(Calendar cal){
+	public void removeObservedCalendar(Calendar cal) {
 		observedCalendars.remove(cal);
 	}
-	
+
 	/**
 	 * Adds a new ID from a observed calendar to be shown.
-	 * @param calId The id to be added to <code>shownObservedCalendars</code>.
+	 * 
+	 * @param calId
+	 *            The id to be added to <code>shownObservedCalendars</code>.
 	 */
-	public void addShownObservedCalendar(long calId){
+	public void addShownObservedCalendar(long calId) {
 		shownObservedCalendars.add(calId);
 	}
-	
+
 	/**
 	 * Remove an Id from <code>shownObservedCalendars</code>.
-	 * @param calId The id to be removed.
+	 * 
+	 * @param calId
+	 *            The id to be removed.
 	 */
-	public void removeShownObservedCalendar(long calId){
+	public void removeShownObservedCalendar(long calId) {
 		shownObservedCalendars.remove(calId);
 	}
-	
+
 	/**
 	 * Check if a Calendar is observed by this User, based on the Calendars id.
-	 * @param calId The id of the Calendar to test if it is observed.
-	 * @return <code>true</code> if <code>observedCalendars</code> contains a Calendar with the same id as the argument.
-	 * <code>false</code> otherwise.
+	 * 
+	 * @param calId
+	 *            The id of the Calendar to test if it is observed.
+	 * @return <code>true</code> if <code>observedCalendars</code> contains a
+	 *         Calendar with the same id as the argument. <code>false</code>
+	 *         otherwise.
 	 */
 	public boolean isCalendarObserved(long calId) {
 		for (Calendar cal : observedCalendars) {
@@ -248,17 +292,20 @@ public class User {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Get all observed Calendars of this User.
+	 * 
 	 * @return list of all observed Calendars of this User.
 	 */
 	public LinkedList<Calendar> getObservedCalendars() {
 		return observedCalendars;
 	}
-	
+
 	/**
-	 * Get a List containing all id's of those observed Calendars which are currently shown.
+	 * Get a List containing all id's of those observed Calendars which are
+	 * currently shown.
+	 * 
 	 * @return list containing all id's of shown observed Calendars.
 	 */
 	public LinkedList<Long> getShownObservedCalendars() {
@@ -267,22 +314,25 @@ public class User {
 
 	/**
 	 * Get the <code>birthdayCalendar</code> of this User.
+	 * 
 	 * @return A Users birthdayCalendar.
 	 */
 	public Calendar getBirthdayCalendar() {
 		return this.birthdayCalendar;
 	}
-	
+
 	/**
 	 * Test if a given Event is this Users birthday.
-	 * @param event The event to be compared.
-	 * @return <code>true</code> if the argument is equal to this Users birthday.
-	 * <code>false</code> otherwise.
+	 * 
+	 * @param event
+	 *            The event to be compared.
+	 * @return <code>true</code> if the argument is equal to this Users
+	 *         birthday. <code>false</code> otherwise.
 	 */
 	public boolean isBirthday(Event event) {
 		return this.birthday.getId() == event.getBaseId();
 	}
-	
+
 	public String getEmailP() {
 		return emailP;
 	}
@@ -328,12 +378,12 @@ public class User {
 	}
 
 	public void setEmailPVis(boolean isEmailPVisible) {
-		this.emailPVis = isEmailPVisible;	
+		this.emailPVis = isEmailPVisible;
 	}
 
 	public void setEmailB(String emailB) {
 		this.emailB = emailB;
-		
+
 	}
 
 	public void setEmailBVis(boolean isEmailBVisible) {
--- calendar/test/UserTest.java
+++ calendar/test/UserTest.java
@@ -13,18 +13,25 @@ public class UserTest extends UnitTest {
 	final static DateTimeFormatter birthdayFormatter = DateTimeFormat
 			.forPattern("dd/MM/yyyy");
 	private User user;
-	private Calendar calendar;
+	private Calendar calendarTest;
 	private DateTime myBirthday = birthdayFormatter.parseDateTime("24/10/1989");
 
 	@Before
 	public void setUp() throws Exception {
 		this.user = new User("hans", "1234", myBirthday, "hansli");
-		this.calendar = new Calendar("Calendar", user);
+		this.calendarTest = new Calendar("CalendarTest", user);
+		user.addCalendar(calendarTest);
 	}
 
 	@Test
 	public void testGetName() {
 		assertEquals("hans", user.getName());
+	}
+
+	@Test
+	public void testSetName() {
+		user.setName("Hans");
+		assertEquals("Hans", user.getName());
 	}
 
 	@Test
@@ -34,8 +41,31 @@ public class UserTest extends UnitTest {
 	}
 
 	@Test
+	public void testSetPassword() {
+		user.setPassword("12345");
+		assertEquals("12345", user.getPassword());
+	}
+
+	@Test
 	public void testIsBirthday() {
 		assertEquals(user.birthday, user.getBirthday());
+
+	}
+
+	@Test
+	public void testSetBirthdayDate() {
+		user.setBirthdayDate(birthdayFormatter.parseDateTime("25/10/1989"));
+		assertEquals(user.birthday, user.getBirthday());
+
+	}
+
+	@Test
+	public void testSetBirthdayPublic() {
+		assertTrue(user.getBirthday().isPrivate());
+		user.setBirthdayPublic(true);
+		assertTrue(user.getBirthday().isPublic());
+		user.setBirthdayPublic(false);
+		assertTrue(user.getBirthday().isPrivate());
 
 	}
 
@@ -50,8 +80,84 @@ public class UserTest extends UnitTest {
 	}
 
 	@Test
+	public void testSetNickname() {
+		user.setNickname("hampuma");
+		assertEquals("hampuma", user.getNickname());
+	}
+
+	@Test
 	public void testGetCalendar() {
 		assertEquals(user.getCalendars().get(0), user.getdefaultCalendar());
+	}
+
+	@Test
+	public void testGetCalendarById() {
+		long id = calendarTest.getId();
+		assertEquals(this.calendarTest, user.getCalendarById(id));
+	}
+
+	@Test
+	public void testAddCalendar() {
+		Calendar newTestCalendar = new Calendar("newTestCalendar", this.user);
+		user.addCalendar(newTestCalendar);
+		assertEquals(newTestCalendar, user.getCalendars().getLast());
+	}
+
+	@Test
+	public void testAddAndRemoveObservedCalendar() {
+		// Create and add a new observed Calendar
+		Calendar newObservedTestCalendar = new Calendar(
+				"newObservedTestCalendar", this.user);
+		user.addObservedCalendar(newObservedTestCalendar);
+		assertEquals(newObservedTestCalendar, user.getObservedCalendars()
+				.getLast());
+		// And then remove again
+		user.removeObservedCalendar(newObservedTestCalendar);
+		assertNotSame(newObservedTestCalendar, user.getObservedCalendars()
+				.getLast());
+	}
+
+	@Test
+	public void testAddAndRemoveShownObservedCalendar() {
+		// Add the Id of the calendar to the shownObservedCalendars LinkedList
+		Calendar newShownObservedTestCalendar1 = new Calendar(
+				"newObservedTestCalendar1", this.user);
+		Calendar newShownObservedTestCalendar2 = new Calendar(
+				"newObservedTestCalendar2", this.user);
+		long calendarId1 = newShownObservedTestCalendar1.getId();
+		long calendarId2 = newShownObservedTestCalendar2.getId();
+		user.addShownObservedCalendar(calendarId1);
+		user.addShownObservedCalendar(calendarId2);
+		assertNotNull(user.getShownObservedCalendars().getFirst());
+		assertSame(calendarId1, user.getShownObservedCalendars().getFirst());
+		assertNotNull(user.getShownObservedCalendars().getLast());
+		assertSame(calendarId2, user.getShownObservedCalendars().getLast());
+		// And remove it again
+		user.removeShownObservedCalendar(calendarId1);
+		assertSame(calendarId2, user.getShownObservedCalendars().getLast());
+
+	}
+
+	@Test
+	public void testIsCalendarObserved() {
+		Calendar newObservedTestCalendar3 = new Calendar(
+				"newObservedTestCalendar3", this.user);
+		long calendarId3 = newObservedTestCalendar3.getId();
+		user.addObservedCalendar(newObservedTestCalendar3);
+		assertFalse(user.isCalendarObserved(000000));
+		assertTrue(user.isCalendarObserved(calendarId3));
+
+	}
+
+	@Test
+	public void testGetBirthdayCalendar() {
+		// hehe ;-) --> mah nüm^
+		assertNotNull(user.getBirthdayCalendar());
+	}
+
+	@Test
+	public void testIsBrithday() {
+		assertTrue(user.isBirthday(user.getBirthday()));
 	}
 
 	@Test
