b4e09f85a0066efb9a36e8e9b2bf4ff281eb2161
Francis Bruehlmann <francis.brue@me.com>
2011-11-09 00:45:15 +0100

    All tests should now be running again Added a few JUnit tests but mostly corrected the selenium tests...

--- calendar/test/CalendarTest.java
+++ calendar/test/CalendarTest.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 
 import models.Calendar;
 import models.Event;
@@ -6,6 +7,7 @@ import models.Event.Visibility;
 import models.User;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,14 +15,23 @@ import play.test.UnitTest;
 
 public class CalendarTest extends UnitTest {
 	private User owner;
-	private Calendar calendar;
+	private User francis;
+	private Calendar calendarOfOwner;
+	// private Calendar calendarOfFrancis;
 	private Event event;
 	private Event repeatingEvent;
+	private Event eventToday;
+	private Event eventToday2;
 
 	@Before
 	public void setUp() throws Exception {
 		this.owner = new User("hans", "123", new DateTime(), "hans1");
-		this.calendar = new Calendar("Calendar", this.owner);
+		this.francis = new User("Francis", "1234", new DateTime(), "francis");
+		/*
+		 * this.calendarOfFrancis = new Calendar("Calendar of Francis",
+		 * this.francis);
+		 */
+		this.calendarOfOwner = new Calendar("Calendar", this.owner);
 		this.event = new Event(owner, new DateTime(0), new DateTime(1),
 				"anEvent", Visibility.PRIVATE, false, 0, owner
 						.getdefaultCalendar().getId(), false);
@@ -30,92 +41,119 @@ public class CalendarTest extends UnitTest {
 	}
 
 	@Test
-	public void testGetName() {
-		assertEquals("Calendar", calendar.getName());
+	public void testGetOwner() {
+		assertEquals(owner, calendarOfOwner.getOwner());
 	}
 
 	@Test
-	public void testGetOwner() {
-		assertEquals(owner, calendar.getOwner());
+	public void testGetName() {
+		assertEquals("Calendar", calendarOfOwner.getName());
 	}
 
 	@Test
 	public void testGetId() {
-		long id = calendar.id;
-		assertEquals(id, calendar.getId());
+		long id = calendarOfOwner.id;
+		assertEquals(id, calendarOfOwner.getId());
+	}
+
+	@Test
+	public void testAddEvent() {
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
+		calendarOfOwner.addEvent(event);
+		assertFalse(calendarOfOwner.getEvents().isEmpty());
+		assertTrue(calendarOfOwner.getEvents().contains(event));
+		assertFalse(calendarOfOwner.getRepeatingEvents().contains(event));
 	}
 
 	@Test
 	public void testGetEventById() {
 		long id = event.getId();
-		calendar.addEvent(event);
-		assertEquals(event, calendar.getEventById(id));
+		calendarOfOwner.addEvent(event);
+		assertEquals(event, calendarOfOwner.getEventById(id));
+	}
+
+	@Test
+	public void testGetDayEvents() {
+		this.eventToday = new Event(owner, new DateTime(), new DateTime(),
+				"eventToday", Visibility.PRIVATE, false, 0, owner
+						.getdefaultCalendar().getId(), false);
+		this.eventToday2 = new Event(owner, new DateTime(), new DateTime(),
+				"eventToday2", Visibility.PUBLIC, false, 0, owner
+						.getdefaultCalendar().getId(), false);
+		calendarOfOwner.addEvent(event);
+		calendarOfOwner.addEvent(eventToday);
+		calendarOfOwner.addEvent(eventToday2);
+		LinkedList<Event> DayEvents = calendarOfOwner.getDayEvents(
+				new LocalDate(), owner);
+		LinkedList<Event> DayEventsFrancis = calendarOfOwner.getDayEvents(
+				new LocalDate(), francis);
+		assertFalse(DayEvents.isEmpty());
+		assertFalse(DayEventsFrancis.isEmpty());
+		assertEquals("[eventToday, eventToday2]", DayEvents.toString());
+		assertEquals("[eventToday2]", DayEventsFrancis.toString());
+		System.out.println(DayEvents);
+		System.out.println(DayEventsFrancis);
 	}
 
 	@Test
 	public void testIterator1() {
-		Iterator<Event> events = calendar.getEventList(new DateTime(0), owner);
+		Iterator<Event> events = calendarOfOwner.getEventList(new DateTime(0),
+				owner);
 		assertNotNull(events);
 	}
 
 	@Test
-	public void testAddEvent() {
-		assertTrue(calendar.getEvents().isEmpty());
-		calendar.addEvent(event);
-		assertFalse(calendar.getEvents().isEmpty());
-		assertTrue(calendar.getEvents().contains(event));
-		assertFalse(calendar.getRepeatingEvents().contains(event));
-	}
-
-	@Test
 	public void testAddRepeatingEvent() {
-		assertTrue(calendar.getEvents().isEmpty());
-		assertTrue(calendar.getRepeatingEvents().isEmpty());
-		calendar.addEvent(repeatingEvent);
-		assertFalse(calendar.getEvents().isEmpty());
-		assertTrue(calendar.getEvents().contains(repeatingEvent));
-		assertTrue(calendar.getRepeatingEvents().contains(repeatingEvent));
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
+		assertTrue(calendarOfOwner.getRepeatingEvents().isEmpty());
+		calendarOfOwner.addEvent(repeatingEvent);
+		assertFalse(calendarOfOwner.getEvents().isEmpty());
+		assertTrue(calendarOfOwner.getEvents().contains(repeatingEvent));
+		assertTrue(calendarOfOwner.getRepeatingEvents()
+				.contains(repeatingEvent));
 	}
 
 	@Test
 	public void testAddToRepeated() {
-		assertTrue(calendar.getEvents().isEmpty());
-		assertTrue(calendar.getRepeatingEvents().isEmpty());
-		calendar.addToRepeated(repeatingEvent);
-		assertTrue(calendar.getEvents().isEmpty());
-		assertTrue(calendar.getRepeatingEvents().contains(repeatingEvent));
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
+		assertTrue(calendarOfOwner.getRepeatingEvents().isEmpty());
+		calendarOfOwner.addToRepeated(repeatingEvent);
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
+		assertTrue(calendarOfOwner.getRepeatingEvents()
+				.contains(repeatingEvent));
 	}
 
 	@Test
 	public void testRemoveEvent() {
-		calendar.addEvent(event);
-		assertTrue(calendar.getEvents().contains(event));
-		calendar.removeEvent(event.getId());
-		assertTrue(calendar.getEvents().isEmpty());
-		assertFalse(calendar.getEvents().contains(event));
+		calendarOfOwner.addEvent(event);
+		assertTrue(calendarOfOwner.getEvents().contains(event));
+		calendarOfOwner.removeEvent(event.getId());
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
+		assertFalse(calendarOfOwner.getEvents().contains(event));
 	}
 
 	@Test
 	public void testRemoveRepeatedEvents() {
-		assertTrue(calendar.getEvents().isEmpty());
-		assertTrue(calendar.getRepeatingEvents().isEmpty());
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
+		assertTrue(calendarOfOwner.getRepeatingEvents().isEmpty());
 		Event repeating1 = repeatingEvent.getNextRepetitionEvent();
 		Event repeating2 = repeating1.getNextRepetitionEvent();
 		Event repeating3 = repeating2.getNextRepetitionEvent();
 		assertEquals(repeatingEvent.getBaseId(), repeating1.getBaseId());
 		assertEquals(repeating1.getBaseId(), repeating2.getBaseId());
 		assertEquals(repeating2.getBaseId(), repeating3.getBaseId());
-		calendar.addEvent(repeatingEvent);
-		calendar.addEvent(repeating1);
-		calendar.addEvent(repeating2);
-		calendar.addEvent(repeating3);
-		assertTrue(calendar.getRepeatingEvents().contains(repeatingEvent));
-		assertTrue(calendar.getRepeatingEvents().contains(repeating1));
-		assertTrue(calendar.getRepeatingEvents().contains(repeating2));
-		assertTrue(calendar.getRepeatingEvents().contains(repeating3));
-		calendar.removeRepeatingEvents(repeating2);
-		assertTrue(calendar.getRepeatingEvents().isEmpty());
-		assertTrue(calendar.getEvents().isEmpty());
+		calendarOfOwner.addEvent(repeatingEvent);
+		calendarOfOwner.addEvent(repeating1);
+		calendarOfOwner.addEvent(repeating2);
+		calendarOfOwner.addEvent(repeating3);
+		assertTrue(calendarOfOwner.getRepeatingEvents()
+				.contains(repeatingEvent));
+		assertTrue(calendarOfOwner.getRepeatingEvents().contains(repeating1));
+		assertTrue(calendarOfOwner.getRepeatingEvents().contains(repeating2));
+		assertTrue(calendarOfOwner.getRepeatingEvents().contains(repeating3));
+		calendarOfOwner.removeRepeatingEvents(repeating2);
+		assertTrue(calendarOfOwner.getRepeatingEvents().isEmpty());
+		assertTrue(calendarOfOwner.getEvents().isEmpty());
 	}
 
 }
