2b862d3c5a978e24bf056839d3f7c34532c80729
Francis Bruehlmann <francis.brue@me.com>
2011-11-15 22:46:53 +0100

    DatabaseTest.java 98.3% Test Coverage

--- calendar/test/DatabaseTest.java
+++ calendar/test/DatabaseTest.java
@@ -12,17 +12,13 @@ import play.test.UnitTest;
 public class DatabaseTest extends UnitTest {
 	DateTimeFormatter dateTimeInputFormatter = DateTimeFormat
 			.forPattern("dd/MM/yyyy, HH:mm");
-	private User user;
 	private User francis;
 	private User stefan;
 	private DateTime today = dateTimeInputFormatter
 			.parseDateTime("15/11/2011, 12:00");
-	private DateTime tomorrow = dateTimeInputFormatter
-			.parseDateTime("16/11/2011, 13:00");
 
 	@Before
 	public void setUp() throws Exception {
-		this.user = new User("hans", "1234", today, "hans2");
 		this.francis = new User("francis", "1234", today, "fran");
 		this.stefan = new User("stefan", "1234", today, "stef");
 
@@ -36,4 +32,60 @@ public class DatabaseTest extends UnitTest {
 		assertTrue(Database.getUserList().contains(francis));
 		assertTrue(Database.getUserList().contains(stefan));
 	}
+
+	@Test
+	public void testAddUserWihtoutObject() {
+		Database.clearDatabase();
+		Database.addUser("Steve", "12345", today, "Jobs");
+		assertEquals("Steve", Database.getUserByName("Steve").getName());
+
+	}
+
+	@Test
+	public void testDeleteUser() {
+		Database.clearDatabase();
+		Database.addUser(francis);
+		Database.addUser(stefan);
+		assertTrue(Database.getUserList().contains(francis));
+		assertTrue(Database.getUserList().contains(stefan));
+		Database.deleteUser("francis", "1234");
+		assertFalse(Database.getUserList().contains(francis));
+		assertTrue(Database.getUserList().contains(stefan));
+	}
+
+	@Test
+	public void testChangePassword() {
+		Database.clearDatabase();
+		Database.addUser(francis);
+		assertTrue(Database.getUserList().contains(francis));
+		Database.changePassword("francis", "1234", "hello");
+		assertEquals("hello", francis.getPassword());
+	}
+
+	@Test
+	public void testSearchUser() {
+		Database.clearDatabase();
+		Database.addUser(francis);
+		assertFalse(Database.searchUser("francis").isEmpty());
+		assertTrue(Database.searchUser("stefan").isEmpty());
+	}
+
+	@Test
+	public void testChangeUserName() {
+		Database.clearDatabase();
+		Database.addUser(francis);
+		this.francis = new User("francisChanged", "1234", today, "fran");
+		Database.changeUserName(francis);
+		assertTrue(Database.getUserList().contains(francis));
+		assertEquals("francisChanged", francis.getName());
+	}
+
+	@Test
+	public void testUserAlreadyRegistrated() {
+		Database.clearDatabase();
+		Database.addUser(francis);
+		assertFalse(Database.userAlreadyRegistrated("stefan"));
+		assertTrue(Database.userAlreadyRegistrated("francis"));
+	}
+
 }
