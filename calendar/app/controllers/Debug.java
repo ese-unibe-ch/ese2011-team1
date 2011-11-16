857b22e5b404a20e045fc8265da7f1f36e10212a
Adrianus Kleemans <a.kleemans@gmail.com>
2011-11-16 15:20:00 +0100

    Made some sketchup for a debugger. Access via http://localhost:9000/Debug/index

---
+++ calendar/app/controllers/Debug.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import models.Calendar;
+import models.Database;
+import models.Event;
+import models.User;
+import play.mvc.Controller;
+
+public class Debug extends Controller {
+
+	public static void index() {
+		List<User> users = Database.getUserList();
+		List<Calendar> calendars = new LinkedList<Calendar>();
+		for (User user : users) {
+			calendars.add(user.getBirthdayCalendar());
+			calendars.addAll(user.getCalendars());
+		}
+
+		List<Event> events = new LinkedList<Event>();
+		for (Calendar cal : calendars) {
+			events.addAll(cal.getEventHeads());
+		}
+
+		render(users, calendars, events);
+	}
+}
