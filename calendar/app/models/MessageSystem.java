99d00f32bf51bda2ec3240a00288700985bb3b23
Michael Single <silent.simplay@gmail.com>
2011-12-21 01:57:04 +0100

    added javadocs, codecleanup

--- calendar/app/models/MessageSystem.java
+++ calendar/app/models/MessageSystem.java
@@ -4,24 +4,27 @@ package models;
 import java.util.ArrayList;
 
 /**
- * message system which is observable by users (we call them listeners). a user can send a message with 
- * a message and a destination user id to the message system. the message system itself
- * delivers this message to the destination user.
+ * message system which is observable by users (we call them listeners). a user
+ * can send a message with a message and a destination user id to the message
+ * system. the message system itself delivers this message to the destination
+ * user.
  * 
  * @author team1
- *
+ * 
  */
 
 public class MessageSystem {
 	private ArrayList<User> listeners = new ArrayList<User>();
 	
 	/**
-	 * notifies the user with id iserId that there is a message for him
-	 * and let him receive this message.
-	 * @param userId id of destination user
-	 * @param message massage for destination user
+	 * notifies the user with id iserId that there is a message for him and let
+	 * him receive this message.
+	 * 
+	 * @param userId
+	 *            id of destination user
+	 * @param message
+	 *            massage for destination user
 	 */
-	
 	public void notifyObservingUser(long targetUserId,long fromUserId, long calendarId, long eventId, String message){
 		for(User user : listeners)
 			if(user.getId() == targetUserId) user.receiveMessage(fromUserId, calendarId, eventId, message);
@@ -30,15 +33,20 @@ public class MessageSystem {
 	
 	/**
 	 * Subscribe a given user to this message system.
-	 * @param user subscribed user
+	 * 
+	 * @param user
+	 *            subscribed user
 	 */
 	public void subscribe(User user){
-		listeners.add(user);
+		if(!listeners.contains(user))
+			listeners.add(user);
 	}
 	
 	/**
 	 * Unsubscribe a given user from this message system
-	 * @param user unsubscribed user
+	 * 
+	 * @param user
+	 *            user to be unsubscribed
 	 */
 	public void unsubscribe(User user){
 		listeners.remove(user);
